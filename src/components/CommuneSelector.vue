d25e086e708545df1e39482cc7d2fb692ee3ee50
wanderfulife <john.wander@hotmail.fr>
2024-01-27 09:32:30 +0100

    gare

--- src/components/CommuneSelector.vue
+++ src/components/CommuneSelector.vue
@@ -1,37 +1,34 @@
 <template>
   <div class="form-group">
-    <input class="form-control" type="text" v-model="searchTerm" @input="showDropdown = true" placeholder="Nom Gare" />
+    <input class="form-control" list="gareList" type="text" v-model="searchTerm" placeholder="Nom Gare"
+      @input="updateGare" />
 
-    <ul v-if="showDropdown && filteredGares.length" class="gare-dropdown">
-      <li v-for="(item, index) in filteredGares" :key="`${item['Code UIC']}-${index}`" @click="selectGare(item)">
-        {{ item['Nom Gare'] }}
-      </li>
-    </ul>
+  <datalist id="gareList">
+    <option v-for="item in filteredGares" :key="item['Code UIC']" :value="item['Nom Gare']">{{ item['Nom Gare'] }}</option>
+  </datalist>
+
   </div>
 </template>
 
-
 <script setup>
-import { ref, computed } from 'vue';
-import gares from "./output.json";
+import { ref, watch, computed } from 'vue';
+import gares from './output.json';
 
-const searchTerm = ref('');
-const showDropdown = ref(false);
+const props = defineProps({
+  modelValue: String
+});
+const emit = defineEmits(['update:modelValue']);
 
-const selectGare = (item) => {
-  searchTerm.value = item['Nom Gare'];
-  showDropdown.value = false;
-};
+const searchTerm = ref(props.modelValue);
+
+watch(searchTerm, (newValue) => {
+  emit('update:modelValue', newValue);
+});
 
 const filteredGares = computed(() => {
-  if (!searchTerm.value) {
-    return [];
-  }
   return gares.filter(item => {
-    const nomGare = item['Nom Gare'] ? item['Nom Gare'].toLowerCase() : '';
+    const nomGare = item['Nom Gare']?.toLowerCase() ?? '';
     return nomGare.includes(searchTerm.value.toLowerCase());
   });
 });
 </script>
-
-
